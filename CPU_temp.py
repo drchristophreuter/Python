b1cfa574ef7f1a10c32f2369b1e3b35ac45ea810
Cris <christoph_reuter@gmx.de>
2024-01-10 11:03:44 +0100

    cpu_temp_test

---
+++ CPU_temp.py
@@ -0,0 +1,15 @@
+import psutil
+
+def get_cpu_temperature():
+    try:
+        temperature = psutil.sensors_temperatures()['coretemp'][0].current
+        return temperature
+    except:
+        return -1
+
+temperature = get_cpu_temperature()
+
+if temperature != -1:
+    print(f"CPU-Temperatur: {temperature}°C")
+else:
+    print("CPU-Temperatur konnte nicht abgerufen werden.")
